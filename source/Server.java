bf82d561b533dc50f5ca5ae5b60d5152a9790a13
Prasad Magdum <153984996+PrasadMagdum08@users.noreply.github.com>
2024-02-09 10:21:47 +0530

    Update Server.java

--- source/Server.java
+++ source/Server.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 public class Server {
+    JFrame frame = new JFrame();
+
+    // frame.setSize(700, 600);
+    frame.setBounds(450,300, 700, 600);
+    
     public static void main(String[] s){
 
     }
