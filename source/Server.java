842cb62641dadddaeb4be7127ed3f4e94bc65bf8
Prasad Magdum <153984996+PrasadMagdum08@users.noreply.github.com>
2024-02-14 13:06:31 +0000

    Updated Server.java

--- source/Server.java
+++ source/Server.java
@@ -1,14 +1,9 @@
-import javax.swing.*;
-// import java.awt.*;
+
 public class Server {
-    JFrame frame = new JFrame();
 
-    // frame.setSize(700, 600);
-    frame.setBounds(450,300, 700, 600);
-    frame.setTitle("DYPCET Chat");
-
-    frame.setVisible(true);
-    public static void main(String[] s){
-
+    
+    public static void main(String[] args){
+        
+        Server server = new Server();
     }
 }
